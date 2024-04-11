ba28b05409b725f8b2881221acddf9bbd3efe6b5
Ronan Corr <ronan_corr@hotmail.com>
2024-04-11 09:22:02 -0400

    convert checkTimerInactivity from jQuery to javaScript

--- assets/js/checkTimerInactivity.js
+++ assets/js/checkTimerInactivity.js
@@ -1,20 +1,22 @@
 let idleTime = 0;
 
-$(document).ready(function () {
-  //Increment the idle time counter every minute.
-  var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+document.addEventListener('DOMContentLoaded', function () {
+  // var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+  setInterval(timerIncrement, 60000); // 1 minute
 
-  //Reset the idle timer on mouse movement or key press
-  $(this).mousemove(function (e) {
+  // Reset the idle timer on mouse movement or key press
+  document.addEventListener('mousemove', function () {
     idleTime = 0;
   });
-  $(this).keypress(function (e) {
+
+  document.addEventListener('keypress', function () {
     idleTime = 0;
   });
 });
 
 function timerIncrement() {
   idleTime++;
+
   if (idleTime === 2) {
     // 2 minutes of inactivity. Redirect to logout page.
     window.location.href = 'logout.php?inactivity=1';
