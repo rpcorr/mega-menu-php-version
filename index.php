bc3703f24ca7b126061199c631761de0d20bd09f
Ronan Corr <ronan_corr@hotmail.com>
2024-04-29 13:47:43 -0400

    uncomment check inactivity script on index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ session_start();
     <script src="./assets/js/scripts.min.js" defer></script>
 
     <?php if (isset($_SESSION['user'])) { ?>
-      <!-- <script src="./assets/js/checkTimerInactivity.min.js" defer></script> -->
+      <script src="./assets/js/checkTimerInactivity.min.js" defer></script>
     <?php } ?>
   </body>
 </html>
