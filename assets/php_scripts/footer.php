b538b9a9649b9fb283c202e6882cf844a113f6ce
Ronan Corr <ronan_corr@hotmail.com>
2024-05-06 13:07:52 -0400

    add a footer to all the pages

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -1,4 +1,7 @@
-<script src="./assets/js/jquery.min.js" defer></script>
+    <footer>
+        <p>Copyright © <?php echo date('Y') ?> Counting Opinions (SQUIRE) Ltd.</p>
+    </footer>
+    <script src="./assets/js/jquery.min.js" defer></script>
     <script src="./assets/js/scripts.min.js" defer></script>
 
     <?php if (isset($_SESSION['user'])) { ?>
