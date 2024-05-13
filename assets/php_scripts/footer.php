1c5df8b6352e7d195e936a325ef985e2a7431380
Ronan Corr <ronan_corr@hotmail.com>
2024-05-13 12:48:18 -0400

    replace Session['loggedIn'] with Session['user'] when determining to add in the check timer script

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -4,7 +4,7 @@
     <script src="./assets/js/jquery.min.js" defer></script>
     <script src="./assets/js/scripts.min.js" defer></script>
 
-    <?php if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === 'Yes')  { ?>
+    <?php if (isset($_SESSION['user'])) { ?>
       <script src="./assets/js/checkTimerInactivity.min.js" defer></script>
     <?php } ?>
   </body>
