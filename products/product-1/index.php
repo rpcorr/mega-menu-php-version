e5ff4f721130970b1fd8913133b2b94bdaec4cc1
Ronan Corr <ronan_corr@hotmail.com>
2024-05-29 11:52:06 -0400

    create a products and product 1 index pages

---
+++ products/product-1/index.php
@@ -0,0 +1,21 @@
+<?php 
+// start the session
+session_start();
+
+$title = "Product 1 - Priority Mega Menu";
+
+include_once( $_SERVER['DOCUMENT_ROOT'] . '\mega-menu\assets\php_scripts\header.php');
+?>
+    <main>
+      <div class="container"> 
+
+        <h1>Product 1</h1>
+         
+        <h2>Section Title</h2>
+          
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum obcaecati voluptates voluptatem facere adipisci aliquam culpa quae aperiam! Nam quis exercitationem dolore excepturi ratione vitae, quidem ipsum ad mollitia iure rem dolorem doloremque voluptate iusto eum vel, veritatis modi expedita dolores facere quos. Distinctio pariatur voluptatibus dolor, velit hic corrupti.</p>
+
+      </div>
+    </main>
+    
+<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '\mega-menu\assets\php_scripts\footer.php'); ?>
