1bdb5deaf4708abbf52484da566ce1dd641e4d75
Ronan Corr <ronan_corr@hotmail.com>
2024-05-29 13:49:47 -0400

    move generate breadcrumbs function to its own file

---
+++ assets/js/generateBreadcrumbs.js
@@ -0,0 +1,64 @@
+function generateBreadcrumbs() {
+    const breadcrumbContainer = document.getElementById('breadcrumb');
+    let pathArray = window.location.pathname.split('/').filter(function (el) {
+      return el.length != 0;
+    });
+  
+    let path = '';
+  
+    const pageTitle = document.querySelector('h1').textContent;
+  
+    // Remove 'index.php' from the array if present
+    pathArray = pathArray.filter((item) => item !== 'index.php');
+  
+    pathArray.forEach((dir, index) => {
+      path += '/' + dir;
+      const isLast = index === pathArray.length - 1;
+  
+      const listItem = document.createElement('li');
+      if (isLast) {
+        listItem.textContent = dir;
+  
+        // if listItem.textContent equals to site root name then change it to Home
+        if (listItem.textContent === 'mega-menu') {
+          listItem.textContent = 'Home > ' + pageTitle;
+        }
+  
+        // if listItem ends with .php, replace the listItem texttContent with the page H1 content
+        if (listItem.textContent.endsWith('.php')) {
+          listItem.textContent = pageTitle;
+        } else {
+          listItem.textContent = capitalizeFirstLetterOfEachWord(
+            listItem.textContent
+          );
+        }
+      } else {
+        const link = document.createElement('a');
+        link.href = path;
+        link.textContent = dir;
+  
+        link.textContent = capitalizeFirstLetterOfEachWord(link.textContent);
+  
+        if (link.textContent === 'mega-menu') {
+          link.textContent = 'Home';
+        }
+  
+        listItem.appendChild(link);
+      }
+  
+      breadcrumbContainer.appendChild(listItem);
+    });
+  }
+  
+  // Call the function to generate breadcrumbs on page load
+  window.onload = generateBreadcrumbs;
+  
+  function capitalizeFirstLetterOfEachWord(str) {
+    return str
+      .toLowerCase()
+      .split(/[\s-_]+/) // Split by spaces, hyphens, or underscores
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(' '); // Join all words with spaces
+  }
