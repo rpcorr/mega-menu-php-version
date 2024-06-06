49b67327cafe39694cd5290f6ebf31b6b0583470
Ronan Corr <ronan_corr@hotmail.com>
2024-06-06 11:46:27 -0400

    only show the menu sections when logged as CO&DEMO

--- index.php
+++ index.php
@@ -24,15 +24,17 @@ if ($_SERVER['DOCUMENT_ROOT'] === 'C:\inetpub\wwwroot') {
         ?>
 
         <?php 
-        
-         $currentURL = remove_last_instance_of_param(get_full_url(), 'sec');
+
+        if( isset($_SESSION['user']) && $_SESSION['user'] == "CO&DEMO") {
+
+          $currentURL = remove_last_instance_of_param(get_full_url(), 'sec');
         ?>
 
-        <h2>Sections</h2>
+          <h2>Sections</h2>
 
-        <p><a href="<?php echo $currentURL ?>">All</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=0">null</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=1">LibSat</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=2">LibPAS</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=5">Inform Us</a>  &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=8">Admin</a></p>
+          <p><a href="<?php echo $currentURL ?>">All</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=0">null</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=1">LibSat</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=2">LibPAS</a> &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=5">Inform Us</a>  &nbsp;&nbsp; <a href="<?php echo $currentURL ?>&sec=8">Admin</a></p>
 
-        
+        <?php } ?>
          
         <h2>Breadcrumb example</h2>
 
