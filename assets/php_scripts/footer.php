984639e60ccbb3f3910c14c36bad9830b584cee3
Ronan Corr <ronan_corr@hotmail.com>
2024-08-07 20:02:27 -0400

    remove jquery file

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -1,7 +1,6 @@
     <footer>
         <p>Copyright © <?php echo date('Y') ?> Counting Opinions (SQUIRE) Ltd.</p>
     </footer>
-    <script src="./assets/js/jquery.min.js" defer></script>
     <script src="./assets/js/scripts.js" defer></script>
 
     <?php if (isset($_SESSION['user'])) { ?>
