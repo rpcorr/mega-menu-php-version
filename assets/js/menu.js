888bc309e1bd7a7797bb5d76c3e03b95a940c44a
Ronan Corr <ronan_corr@hotmail.com>
2024-11-06 16:26:42 -0500

    stash custom reports under custom report heading

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -24,16 +24,39 @@ if (!ukey) {
 }
 
 // Local host
+JSONfile = 'http://localhost/mega-menu/assets/json/';
 if (!ukey && !user) {
-  JSONfile = 'http://localhost/mega-menu/assets/json/co-pages.json';
+  JSONfile += 'co-pages.json';
 } else {
   if (ukey) {
     if (ukey === 'b5e79c05b3f12219e725fc167edefdd1')
-      JSONfile =
-        'http://localhost/mega-menu/assets/json/co-pages-logged-in.json';
-    else
-      JSONfile =
-        'http://localhost/mega-menu/assets/json/barry-hill-central-CM.json';
+      JSONfile += 'co-pages-logged-in.json';
+    else if (ukey === '23dd36999727c42c207a0445304f44e7')
+      JSONfile += 'barry-hill-system-su-cm.json';
+    else if (ukey === 'dcbe3c8f358ec7e3e082544cd379a5d6')
+      JSONfile += 'barry-hill-central-cm.json';
+    else if (ukey === '8b608f21a5f4d8e3ce66a6a74f1e7419')
+      JSONfile += 'barry-hill-central-da.json';
+    else if (ukey === '63a5c03a3135a36c54b5a7dcd65bd3f2')
+      JSONfile += 'barry-hill-central-da.json';
+    else if (ukey === '3a8a2b2cb83baa1d39117771446beae9')
+      JSONfile += 'barry-hill-central-dm.json';
+    else if (ukey === 'aa6ac353aef93145142cdb34e62fabab')
+      JSONfile += 'barry-hill-central-em.json';
+    else if (ukey === '130d58f57067ec3fc9669d7075b122ca')
+      JSONfile += 'barry-hill-central-lm.json';
+    else if (ukey === 'f3447c09021898f7f22b88b2d729b3d1')
+      JSONfile += 'barry-hill-central-rm.json';
+    else if (ukey === 'a0e5eee003ae07cd6f9bf508b860fc19')
+      JSONfile += 'barry-hill-central-rv.json';
+    else if (ukey === 'b7aa6d97e31cbff74509626b4c6581af')
+      JSONfile += 'belleview-system-su-cm.json';
+    else if (ukey === 'd47e87449b72a5affca017ad90c314c0')
+      JSONfile += 'belleview-informsus-de-rm.json';
+    else if (ukey === '452d5aa940f209f0018444f14204a714')
+      JSONfile += 'belleview-libpas-dm-rv.json';
+    else if (ukey === 'b0b95b071bc61c3a524984d8c2810d0f')
+      JSONfile += 'belleview-libsat-de-da-rm.json';
   } else if (user) {
     JSONfile = 'http://localhost/mega-menu/assets/json/admin.json';
   }
@@ -126,8 +149,6 @@ document.addEventListener('DOMContentLoaded', () => {
           // filter out all entries that has maphat in it's page.prompt and store in variable maphatEntries; and all the others in a variable named libSatPagesNoMAPHAT
           const regex = /maphat/i;
 
-          console.log('LibSatPages are present');
-          console.log(`libSatPages: ${libSatPages[0]}`);
           const maphatEntries = libSatPages[0].pages.filter((page) =>
             regex.test(page.page_prompt)
           );
@@ -146,6 +167,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
             let iSubMenuLevels = 0;
             let bDoubleMenu = false;
+            let bDoubleMenuRemainsOpen = false;
+            let bNPSPresent = false;
 
             // loop through LibSat Pages
             for (let i = 0; i <= libSatPagesNoMAPHAT.length; i++) {
@@ -167,11 +190,18 @@ document.addEventListener('DOMContentLoaded', () => {
                       bDoubleMenu = true;
                     }
 
-                    if (!bDoubleMenu)
+                    if (!bDoubleMenu) {
+                      // close custom Reports menu
+                      if (libSatPagesNoMAPHAT[i].page_prompt === 'NPS') {
+                        strLibSatMenuStructure += '</ul></li>';
+                        bNPSPresent = true;
+                      }
+
                       strLibSatMenuStructure += openLibSatSubMenu(
                         libSatPagesNoMAPHAT,
                         i
                       );
+                    }
 
                     if (bDoubleMenu) {
                       strDoubleMenu += `
@@ -211,6 +241,7 @@ document.addEventListener('DOMContentLoaded', () => {
                       // update i
                       i = i + iHowManyItems + 2 - 1;
 
+                      // close out double menu
                       strDoubleMenu += `</ul>
                             </li>
                           </ul>
@@ -252,16 +283,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
                       // 5: update i
                       i = i + iHowManyItems - 1;
+
+                      // add closing unorder list and it's menu
+                      //if double menu should remain open
+                      if (bDoubleMenuRemainsOpen) {
+                        strLibSatMenuStructure += '</ul></li>';
+                      }
                     }
                   }
 
-                  if (iSubMenuLevels > 0 && !bDoubleMenu)
-                    strLibSatMenuStructure += '</ul></li>';
+                  // if page_prompt equals Custom Report then double menu remains open
+                  if (libSatPagesNoMAPHAT[i].page_prompt === 'Custom Report') {
+                    bDoubleMenuRemainsOpen = true;
+                  }
 
+                  // attach opening of double menu to libSat menu structure
                   if (bDoubleMenu && strDoubleMenu !== '') {
                     strLibSatMenuStructure += strDoubleMenu;
                   }
 
+                  // add closing unorder list and it's menu
+                  // if not double menu and not should remain open but has submenu levels
+                  if (
+                    iSubMenuLevels > 0 &&
+                    !bDoubleMenu &&
+                    !bDoubleMenuRemainsOpen
+                  )
+                    strLibSatMenuStructure += '</ul></li>';
+
+                  // reset bDoubleMenu and iSubMenuLevels
                   if (bDoubleMenu) {
                     bDoubleMenu = false;
                     iSubMenuLevels = 0;
@@ -269,6 +319,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
               }
             }
+
+            // if NPS menu is not present include closing </ul></li>
+            if (!bNPSPresent) strLibSatMenuStructure += '</ul></li>';
 
             // add MAPHAT entries
             let strMapHatMenuStructure = '';
@@ -401,6 +454,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (strLibSatMenuStructure !== '' && strLibSatMenuStructure !== undefined)
         menuHTML += strLibSatMenuStructure;
+
+      console.log(menuHTML);
 
       document.getElementById('menu-main-menu').innerHTML = menuHTML;
 
