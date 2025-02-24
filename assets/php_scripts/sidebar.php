7491c0e527890bfbe931e0d0722a35e6a4c86866
Ronan Corr <ronan_corr@hotmail.com>
2025-02-24 11:42:49 -0500

    add aria-label to sidebar navigation

--- assets/php_scripts/sidebar.php
+++ assets/php_scripts/sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar" class="sidebar-container">
-    <nav class="sidebar">
+    <nav class="sidebar" aria-label="Sidebar">
         <ul>
             <li><a href="#" style="color: white; text-decoration: none;">Menu Item 1</a></li>
             <li><a href="#" style="color: white; text-decoration: none;">Menu Item 2</a></li>
