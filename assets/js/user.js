e9363c004d075addeccebbda92b0f9f5e7737a51
Ronan Corr <ronan_corr@hotmail.com>
2025-05-29 16:17:37 -0400

    dynamically generate menu users

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,48 +1,53 @@
 'use strict';
-let JSONfile = '';
-
-// Counting Opinions server
-if (!ukey) {
-  JSONfile = 'https://dev.countingopinions.com/ws/portal/get_pages.php?is_menu';
-} else {
-  JSONfile = `https://dev.countingopinions.com/ws/portal/get_pages.php?is_menu&portal=${portal}&ukey=${ukey}`;
-}
 
 // Local host
-JSONfile = 'http://localhost/mmenu/assets/json/';
+const baseURL = 'http://localhost/mmenu/assets/json/';
+const ukeyUserMap = {
+  b5e79c05b3f12219e725fc167edefdd1: 'co-demo.json',
+  aeff1d4962b1fb2aa59a472d2df7efa1: 'cm-demo.json',
+  b312f517b7bcfe3177d6cb4da6922884: 'co-paul.json',
+  '0be2b1990297545d657155c2de0e62e4': 'co-cole-t.json',
+  '5a0dedbcf6da1edccf630ba76699e226': 'co-ala.json',
+  '3a48a031ac973365980798360b6fdd7e': 'de-demo.json',
+  bd13b136ae6ad94703cc1bf44c0201d8: 'rm-demo.json',
+  '612879a086be69a0bac8d14445ef9ae5': 'rv-demo.json',
+  '5544a5c002d3646ea44736a30eeef446': 'tc-demo.json',
+  '23dd36999727c42c207a0445304f44e7': 'barry-hill-system-su-cm.json',
+  dcbe3c8f358ec7e3e082544cd379a5d6: 'barry-hill-central-cm.json',
+  '8b608f21a5f4d8e3ce66a6a74f1e7419': 'barry-hill-central-da.json',
+  '63a5c03a3135a36c54b5a7dcd65bd3f2': 'barry-hill-central-da.json',
+  '3a8a2b2cb83baa1d39117771446beae9': 'barry-hill-central-dm.json',
+  aa6ac353aef93145142cdb34e62fabab: 'barry-hill-central-em.json',
+  '130d58f57067ec3fc9669d7075b122ca': 'barry-hill-central-lm.json',
+  f3447c09021898f7f22b88b2d729b3d1: 'barry-hill-central-rm.json',
+  a0e5eee003ae07cd6f9bf508b860fc19: 'barry-hill-central-rv.json',
+  b7aa6d97e31cbff74509626b4c6581af: 'belleview-system-su-cm.json',
+  d47e87449b72a5affca017ad90c314c0: 'belleview-informsus-de-rm.json',
+  '452d5aa940f209f0018444f14204a714': 'belleview-libpas-dm-rv.json',
+  b0b95b071bc61c3a524984d8c2810d0f: 'belleview-libsat-de-da-rm.json',
+  '837a12e67c48dae89c05bf746b9684bc': 'co-democa.json',
+  '11a795a3f4846e809b675b6fec58e941': 'co-democa.json',
+  '695df2b1afc3dee4c6690a30f63abcf7': 'riversideSystem-rm-de-cm.json',
+};
+
+let pagesJSONfile = baseURL;
+
 if (!ukey && !user) {
-  JSONfile += 'co-pages.json';
+  pagesJSONfile += 'co-pages.json';
 } else {
-  if (ukey) {
-    if (ukey === 'b5e79c05b3f12219e725fc167edefdd1')
-      JSONfile += 'co-pages-logged-in.json';
-    else if (ukey === '23dd36999727c42c207a0445304f44e7')
-      JSONfile += 'barry-hill-system-su-cm.json';
-    else if (ukey === 'dcbe3c8f358ec7e3e082544cd379a5d6')
-      JSONfile += 'barry-hill-central-cm.json';
-    else if (ukey === '8b608f21a5f4d8e3ce66a6a74f1e7419')
-      JSONfile += 'barry-hill-central-da.json';
-    else if (ukey === '63a5c03a3135a36c54b5a7dcd65bd3f2')
-      JSONfile += 'barry-hill-central-da.json';
-    else if (ukey === '3a8a2b2cb83baa1d39117771446beae9')
-      JSONfile += 'barry-hill-central-dm.json';
-    else if (ukey === 'aa6ac353aef93145142cdb34e62fabab')
-      JSONfile += 'barry-hill-central-em.json';
-    else if (ukey === '130d58f57067ec3fc9669d7075b122ca')
-      JSONfile += 'barry-hill-central-lm.json';
-    else if (ukey === 'f3447c09021898f7f22b88b2d729b3d1')
-      JSONfile += 'barry-hill-central-rm.json';
-    else if (ukey === 'a0e5eee003ae07cd6f9bf508b860fc19')
-      JSONfile += 'barry-hill-central-rv.json';
-    else if (ukey === 'b7aa6d97e31cbff74509626b4c6581af')
-      JSONfile += 'belleview-system-su-cm.json';
-    else if (ukey === 'd47e87449b72a5affca017ad90c314c0')
-      JSONfile += 'belleview-informsus-de-rm.json';
-    else if (ukey === '452d5aa940f209f0018444f14204a714')
-      JSONfile += 'belleview-libpas-dm-rv.json';
-    else if (ukey === 'b0b95b071bc61c3a524984d8c2810d0f')
-      JSONfile += 'belleview-libsat-de-da-rm.json';
+  if (ukey && ukeyUserMap[ukey]) {
+    pagesJSONfile += ukeyUserMap[ukey];
   } else if (user) {
-    JSONfile = 'http://localhost/mmenu/assets/json/admin.json';
+    pagesJSONfile += 'admin.json';
   }
 }
+
+//let pagesJSONfile = '';
+
+// Counting Opinions server
+// if (!ukey) {
+//   pagesJSONfile =
+//     'https://dev.countingopinions.com/ws/portal/get_pages.php?is_menu';
+// } else {
+//   pagesJSONfile = `https://dev.countingopinions.com/ws/portal/get_pages.php?is_menu&portal=${portal}&ukey=${ukey}`;
+// }
--- menu-users.php
+++ menu-users.php
@@ -33,26 +33,45 @@ include('assets/php_scripts/header.php');
             echo '<p><a href="mmenu.php">Back</a></p>';
         }
         ?>
-        <div style="display:flex; justify-content: center; align-items: center; flex-wrap: wrap; gap: 100px;">
-          <div>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=b5e79c05b3f12219e725fc167edefdd1&user=Admin">Admin</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=23dd36999727c42c207a0445304f44e7&user=BarryHillCentral_SU_CM">BarryHillSystem_SU_CM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=dcbe3c8f358ec7e3e082544cd379a5d6&user=BarryHillCentral_CM">BarryHillCentral_CM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=8b608f21a5f4d8e3ce66a6a74f1e7419&user=BarryHillCentral_DA">BarryHillCentral_DA</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=63a5c03a3135a36c54b5a7dcd65bd3f2&user=BarryHillCentral_DE">BarryHillCentral_DE</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=3a8a2b2cb83baa1d39117771446beae9&user=BarryHillCentral_DM">BarryHillCentral_DM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=aa6ac353aef93145142cdb34e62fabab&user=BarryHillCentral_EM">BarryHillCentral_EM</a></p>
+        <script>
+          const portalTemp = <?php echo json_encode($portal); ?>;
+          const userTemp = <?php echo json_encode($user); ?>;
+
+          let userJSONfile = '';
+
+          if (portalTemp.toLowerCase() === 'democa')
+            userJSONfile = 'http://localhost/mmenu/assets/json/users-democa.json';
+          else 
+            userJSONfile = 'http://localhost/mmenu/assets/json/users-demo.json';
+
+          fetch(userJSONfile)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json(); // Parse the JSON from the response
+          })
+          .then(data => {
+            
+            const usersDiv = document.getElementById('users');
+  
+            data.forEach(user => {
+                const p = document.createElement('p');
+                const a = document.createElement('a');
+                a.href = `menu-users.php?is_menu&portal=${portal}&ukey=${user.ukey}&user=${encodeURIComponent(user.username)}`;
+                a.textContent = user.username;
+                p.appendChild(a);
+                usersDiv.appendChild(p);
+
+            });
+          })
+          .catch(error => {
+            console.error('There was a problem fetching the JSON file:', error);
+          });
+        </script>
+          <div class="users-flex">
+            <div id="users" class="users-grid"></div>
           </div>
-          <div>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=130d58f57067ec3fc9669d7075b122ca&user=BarryHillCentral_LM">BarryHillCentral_LM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=f3447c09021898f7f22b88b2d729b3d1&user=BarryHillCentral_RM">BarryHillCentral_RM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=a0e5eee003ae07cd6f9bf508b860fc19&user=BarryHillCentral_RV">BarryHillCentral_RV</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=b7aa6d97e31cbff74509626b4c6581af&user=BelleviewSystem_SU_CM">BelleviewSystem_SU_CM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=d47e87449b72a5affca017ad90c314c0&user=BelleView_InformsUs_DE_RM">BelleView_InformsUs_DE_RM</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=452d5aa940f209f0018444f14204a714&user=Belleview_LibPAS_DM_RV">Belleview_LibPAS_DM_RV</a></p>
-            <p><a href="menu-users.php?is_menu&portal=demo&ukey=b0b95b071bc61c3a524984d8c2810d0f&user=Belleview_LibSat_DE_DA_RM">Belleview_LibSat_DE_DA_RM</a></p>
-          </div>
-        </div>
     <?php } ?>
   </div>
 </main>
