0899896528d2980f0d489f6803974f76841a0178
Ronan Corr <ronan_corr@hotmail.com>
2025-06-09 07:25:33 -0400

    if $user is not set; set $user to CO&DEMO

--- menu-users.php
+++ menu-users.php
@@ -12,8 +12,10 @@ include('assets/php_scripts/header.php');
     <div class="container">
       <h1>Menu Users</h1>
 
-      <h2 style="text-align:center">Current User: <?php echo $user ?> </h2>
+      <?php if (!isset($user)) { $user = 'CO&DEMO'; } ?>
 
+      <h2 style="text-align:center">Current User: <?php echo $user ?> </h2>
+      
       <?php 
       
       // if ukey is present, display preference link
