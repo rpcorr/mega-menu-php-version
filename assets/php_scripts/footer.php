e04ad19c0d314bfb3ab69978e4dc60f69277ffd6
Ronan Corr <ronan_corr@hotmail.com>
2025-06-09 07:11:39 -0400

    change Admin to CO&DEMO

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -4,7 +4,7 @@
       const ukey = '<?php echo $ukey; ?>'; 
       const portal = <?php echo json_encode($portal); ?>;
       const queryString = <?php echo json_encode($queryString); ?>;
-      const user = <?php echo json_encode($user); ?> === null ? 'Admin' : <?php echo json_encode($user); ?>;
+      const user = <?php echo json_encode($user); ?> === null ? 'CO&DEMO' : <?php echo json_encode($user); ?>;
 
       console.log(`I am outside the menu.js.  Ukey is ${ukey}.  Portal is ${portal}.`);
       console.log(`I am outside of breadcrumbs.js. Querystring is ${queryString}`);
