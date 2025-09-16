76f9e98d6ced7232d1661a9acc18de53a089a4a5
Ronan Corr <ronan_corr@hotmail.com>
2025-09-16 11:24:09 -0400

    update hamburger query selector from .hamburger to .sidebar-hamburger

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -117,7 +117,7 @@ function toggleSidebar() {
   const links = sidebar.querySelectorAll(
     'a, button, input, [tabindex]:not([tabindex="-1"])'
   );
-  const hamburger = button.querySelector('.hamburger');
+  const hamburger = button.querySelector('.sidebar-hamburger');
   const announcement = document.getElementById('sidebar-announcement');
 
   const isOpen = button.classList.contains('open');
