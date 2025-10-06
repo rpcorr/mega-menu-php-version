a0fdd51511da36330ae86a86c26224c14174e9df
Ronan Corr <ronan_corr@hotmail.com>
2025-10-06 19:29:44 -0400

    move PHP code before DOCTYPE

--- assets/php_scripts/header.php
+++ assets/php_scripts/header.php
@@ -1,82 +1,82 @@
+<?php
+session_start();
+
+
+// determine which variable to use: Request or Cookie
+$ukey = '';
+$portal = '';
+
+// assign $_SESSION['portal_name'] to $portal if present
+if ($_SESSION['portal_name'] !== '') {
+    $portal = $_SESSION['portal_name'];
+}
+
+// assign $_COOKIE['ukey'] to $ukey if present
+if ($_COOKIE['ukey']) {
+    $ukey = $_COOKIE['ukey'];
+}
+
+// assign $_REQUEST['ukey'] to $ukey and $_REQUEST['portal'] to $portal if present
+if ($_REQUEST['ukey'] && $_REQUEST['portal'] ) {
+    $ukey = $_REQUEST['ukey'];
+    $portal = $_REQUEST['portal'];
+}
+
+if ($_REQUEST['user']) {
+  $user = $_REQUEST['user'];
+}
+
+include('session_check.php');
+        
+// Function to get the full URL of the current page
+function getFullUrl() {
+  $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+  $host = $_SERVER['HTTP_HOST'];
+  $uri = $_SERVER['REQUEST_URI'];
+  return $protocol . $host . $uri;
+}
+
+// Get the full URL
+$fullUrl = getFullUrl();
+
+// Parse the URL and get the query string
+$parsed_url = parse_url($fullUrl);
+
+// retrieve the query string
+$queryString = $parsed_url['query'];
+
+
+function getRelativePath($targetPath) {
+  // Get the current script's directory
+  $currentDir = dirname($_SERVER['SCRIPT_NAME']);
+  
+  // Split the directories into an array
+  $currentDirParts = explode('/', trim($currentDir, '/'));
+  
+  // Count the number of directories
+  $depth = count($currentDirParts);
+  
+  // Generate the relative path prefix
+  $relativePath = str_repeat('../', $depth-1);
+  
+  // Concatenate the target path
+  $relativePath = rtrim($relativePath, '/') . '/' . ltrim($targetPath, '/');
+  
+  if ($relativePath === '/') $relativePath = '';
+  return $relativePath;
+}
+
+  // Allow current user to switch users
+  $_SESSION['switchAble'] = true;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
-    <?php
-    session_start();
-
-
-    // determine which variable to use: Request or Cookie
-    $ukey = '';
-    $portal = '';
-
-    // assign $_SESSION['portal_name'] to $portal if present
-    if ($_SESSION['portal_name'] !== '') {
-        $portal = $_SESSION['portal_name'];
-    }
-
-    // assign $_COOKIE['ukey'] to $ukey if present
-    if ($_COOKIE['ukey']) {
-        $ukey = $_COOKIE['ukey'];
-    }
-
-    // assign $_REQUEST['ukey'] to $ukey and $_REQUEST['portal'] to $portal if present
-    if ($_REQUEST['ukey'] && $_REQUEST['portal'] ) {
-        $ukey = $_REQUEST['ukey'];
-        $portal = $_REQUEST['portal'];
-    }
-
-    if ($_REQUEST['user']) {
-      $user = $_REQUEST['user'];
-    }
-
-    include('session_check.php');
-            
-    // Function to get the full URL of the current page
-    function getFullUrl() {
-      $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-      $host = $_SERVER['HTTP_HOST'];
-      $uri = $_SERVER['REQUEST_URI'];
-      return $protocol . $host . $uri;
-    }
     
-    // Get the full URL
-    $fullUrl = getFullUrl();
-
-    // Parse the URL and get the query string
-    $parsed_url = parse_url($fullUrl);
-
-    // retrieve the query string
-    $queryString = $parsed_url['query'];
-    
-    
-    function getRelativePath($targetPath) {
-      // Get the current script's directory
-      $currentDir = dirname($_SERVER['SCRIPT_NAME']);
-      
-      // Split the directories into an array
-      $currentDirParts = explode('/', trim($currentDir, '/'));
-      
-      // Count the number of directories
-      $depth = count($currentDirParts);
-      
-      // Generate the relative path prefix
-      $relativePath = str_repeat('../', $depth-1);
-      
-      // Concatenate the target path
-      $relativePath = rtrim($relativePath, '/') . '/' . ltrim($targetPath, '/');
-      
-      if ($relativePath === '/') $relativePath = '';
-      return $relativePath;
-    }
-
-      // Allow current user to switch users
-      $_SESSION['switchAble'] = true;
-
-    ?>
-
     <!-- stylesheets -->
     <link rel="stylesheet" type="text/css" href="<?php echo getRelativePath(''); ?>assets/css/reset.min.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo getRelativePath(''); ?>assets/css/navigation-menu.css" />
