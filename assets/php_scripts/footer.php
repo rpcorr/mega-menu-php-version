d8ba1902a443c97aea79b6730aeef34e9d184948
Ronan Corr <ronan_corr@hotmail.com>
2025-10-17 17:50:15 -0400

    add alt attrib to <img in templates tags

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -75,18 +75,18 @@
 
     <!-- ///// The Templates ///// -->
     <template id="menuTemplate">
-      <li role="presentation"><img src="" /><p><a href="" role="tab"><strong></strong><br/><span></span></a></p> <div class="circle">
+      <li role="presentation"><img src="" alt="" /><p><a href="" role="tab"><strong></strong><br/><span></span></a></p> <div class="circle">
           <div class="caret"></div>
         </div></li>
     </template> 
 
     <template id="oneMenuTemplate">
-      <li><img src="" /><p><a href="" role="tab" aria-selected="true"><strong></strong><br/><span></span></a></p> </li>
+      <li><img src="" alt="" /><p><a href="" role="tab" aria-selected="true"><strong></strong><br/><span></span></a></p> </li>
     </template> 
 
     <template id="menuContent">
       <div role="listitem">
-        <img src="" width="" alt="" height=""  />
+        <img src="" width="" height="" alt=""  />
         <p><a href="#"><strong></strong><br/><span></span></a></p>
       </div>
     </template>
@@ -94,7 +94,7 @@
     <template id="menuExtraContent">
       <div class="extra-content">
         <div>
-          <img src="" width="" height="" align="left" />
+          <img src="" width="" height="" align="left" alt="" />
           <p>
             <strong></strong>
           </p>
