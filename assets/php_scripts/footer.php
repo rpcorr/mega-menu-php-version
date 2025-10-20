1c3095e3a399d78ec12190f83063c6adf9fb5224
Ronan Corr <ronan_corr@hotmail.com>
2025-10-20 17:34:34 -0400

    remove js/selectTheme.js from footer and add it to menu.js

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -41,7 +41,6 @@
     <script src='<?php echo getRelativePath(''); ?>assets/js/user.js' defer></script>
     <script src='<?php echo getRelativePath(''); ?>assets/js/menu.js' defer></script>
     <?php if ($_COOKIE['ukey'] || $_REQUEST['ukey']) { ?>
-      <script src='<?php echo getRelativePath(''); ?>assets/js/selectTheme.js' defer></script>
     <!-- <script src='<?php //echo getRelativePath(''); ?>assets/js/checkTimerInactivity.min.js' defer></script> -->
     <?php } ?>
 
