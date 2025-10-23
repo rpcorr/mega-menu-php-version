629c98cf6399857456413b97617fd81524d040bd
Ronan Corr <ronan_corr@hotmail.com>
2025-10-23 14:35:26 -0400

    save a copy of sessionCheck.js beforer deleting from server

---
+++ assets/references/on-server/assets/js/sessionCheck.js
@@ -0,0 +1,11 @@
+//check for ukey
+if (!ukey) {
+  // return to previous page
+  // window.addEventListener('load', function () {
+  //   window.history.back();
+  // });
+  //console.log('go back');
+  //window.history.back();
+  let previousUrl = window.location;
+  console.log(previousUrl);
+}
