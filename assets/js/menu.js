5f38145b07a3b6d2e2276acbb41f6b99c7bd9459
Ronan Corr <ronan_corr@hotmail.com>
2025-10-24 17:41:13 -0400

    move skipMenu anchor from header.php to menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -127,6 +127,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // render header
   renderHeader();
 
+  insertSkipMenuAnchor();
+
   // iniltialize menu templates
   initMenuTemplates();
 
@@ -3124,4 +3126,17 @@ function injectNavigationMenuCSS() {
   link.href = `${relPath}assets/css/navigation-menu.css`;
 
   document.head.appendChild(link);
+}
+
+function insertSkipMenuAnchor() {
+  const skip = document.createElement('a');
+  skip.id = 'skipMenu';
+  skip.className = 'screen-reader-text';
+
+  const main = document.querySelector('main');
+  if (main) {
+    main.parentNode.insertBefore(skip, main);
+  } else {
+    console.warn('<main> not found in document');
+  }
 }
