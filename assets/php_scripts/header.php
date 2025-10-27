2eb8bbeb54e2d9fd522267f11992f78c43aff55e
Ronan Corr <ronan_corr@hotmail.com>
2025-10-27 00:10:44 -0400

    remove const basePath variable

--- assets/php_scripts/header.php
+++ assets/php_scripts/header.php
@@ -58,6 +58,5 @@ else $_SESSION['switchAble'] = false;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
-    <script>//const basePath = "<?php //echo getRelativePath(''); ?>";</script>
 </head>
 <body>
