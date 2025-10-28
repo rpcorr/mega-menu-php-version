da37b78647d2dbab29ad6f41d33a54f87512d024
Ronan Corr <ronan_corr@hotmail.com>
2025-10-28 18:53:02 -0400

    move menu.js and user-pages.js out of assets and in widgets/menu

--- assets/php_scripts/footer.php
+++ assets/php_scripts/footer.php
@@ -61,7 +61,7 @@
       console.log(`I am outside of breadcrumbs.js. Querystring is ${queryString}`);
       
     </script>
-    <script src='<?php echo getRelativePath(''); ?>assets/js/user-pages.js' defer></script>
-    <script src='<?php echo getRelativePath(''); ?>assets/js/menu.js' defer></script>
+    <script src='<?php echo getRelativePath(''); ?>widgets/menu/user-pages.js' defer></script>
+    <script src='<?php echo getRelativePath(''); ?>widgets/menu/menu.js' defer></script>
   </body>
 </html>
