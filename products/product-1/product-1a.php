3674cf03fd327c611faae9c1fc5859fd4282725e
Ronan Corr <ronan_corr@hotmail.com>
2025-11-04 01:35:31 -0500

    place session_check code into pages and delete the include file

--- products/product-1/product-1a.php
+++ products/product-1/product-1a.php
@@ -30,7 +30,47 @@ if (isset($_REQUEST['user'])) {
     $user = $_REQUEST['user'];
 }
 
-include(__DIR__ . '/session_check.php');
+//////////////// Session check start /////////////////////
+
+// Get the current page URL
+$currentUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$redirectUrl;
+// prevent updating $redirectUrl value if page is refreshed
+if (isset($_SESSION['previousUrl']) && $_SESSION['previousUrl'] !== $currentUrl) {
+    $redirectUrl = $_SESSION['previousUrl'];
+}
+ else {
+    $redirectUrl = isset($_SESSION['redirectUrl']) ? $_SESSION['redirectUrl'] : '';
+}
+
+if (!isset($_SESSION['previousUrl'])) {
+    // set $_SESSION['previousUrl']
+    $_SESSION['previousUrl'] = $currentUrl;
+
+}
+
+if ($currentUrl !== $_SESSION['previousUrl']) {
+    // update session variable
+    $_SESSION['redirectUrl'] = $_SESSION['previousUrl'];
+    $_SESSION['previousUrl'] = $currentUrl;
+}
+
+// check if there is a ukey or user query param and page is protected.
+if (($ukey == "" && $_REQUEST['user'] == "") && $protected) {
+
+    // Check if the URL contains '?user='
+    if (strpos($_SESSION['previousUrl'], '?user=') !== false) {
+
+        // Remove query parameters
+        $_SESSION['previousUrl'] = strtok($_SESSION['previousUrl'], '?');
+    }
+
+    // redirect to previous logged out page
+    header("Location:" . $redirectUrl);
+    exit; // Make sure to exit after redirection to prevent further execution
+}
+
+//////////////// Session check end //////////////////////
 
 // Function to get the full URL of the current page
 function getFullUrl() {
