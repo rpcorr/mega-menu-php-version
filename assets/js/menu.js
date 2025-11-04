d32adbc98553eef020996ee5ec904d5058a8728b
Ronan Corr <ronan_corr@hotmail.com>
2025-11-04 00:56:35 -0500

    move switchable, sidebar, and select theme widgets out from assets and into widgets under root

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,7 +8,7 @@ const prefix = getRelativePath();
 
 ////////////////////////////////////////////////////
 // Widgets file paths needs to be defined here
-const widgetsPath = `${prefix}assets/widgets/`;
+const widgetsPath = `${prefix}widgets/`;
 
 const resetCssPath = `${prefix}assets/css/reset.min.css`;
 const switchableJsPath = `${widgetsPath}switchable/switchable.js`;
