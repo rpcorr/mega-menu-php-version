0cb755bc0165f397aecb9038875012b01cd2571b
Ronan Corr <ronan_corr@hotmail.com>
2025-11-04 01:08:14 -0500

    place header in all files - no includes

--- mmenu.php
+++ mmenu.php
@@ -2,9 +2,67 @@
 
 $title = "Welcome - Counting Opinions";
 
-include(__DIR__ . '/assets/php_scripts/header.php');
+session_start();
+
+// determine which variable to use: Request or Cookie
+$ukey = '';
+$portal = '';
+$user = '';
+
+// assign $_SESSION['portal_name'] to $portal if present
+if (isset($_SESSION['portal_name']) && $_SESSION['portal_name'] !== '') {
+    $portal = $_SESSION['portal_name'];
+}
+
+// assign $_COOKIE['ukey'] to $ukey if present
+if (isset($_COOKIE['ukey'])) {
+    $ukey = $_COOKIE['ukey'];
+}
+
+// assign $_REQUEST['ukey'] to $ukey and $_REQUEST['portal'] to $portal if present
+if (isset($_REQUEST['ukey']) && isset($_REQUEST['portal'])) {
+    $ukey = $_REQUEST['ukey'];
+    $portal = $_REQUEST['portal'];
+}
+
+// assign $_REQUEST['user'] to $user if present
+if (isset($_REQUEST['user'])) {
+    $user = $_REQUEST['user'];
+}
+
+include(__DIR__ . '/session_check.php');
+
+// Function to get the full URL of the current page
+function getFullUrl() {
+  $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+  $host = $_SERVER['HTTP_HOST'];
+  $uri = $_SERVER['REQUEST_URI'];
+  return $protocol . $host . $uri;
+}
+
+// Get the full URL
+$fullUrl = getFullUrl();
+
+// Parse the URL and get the query string
+$parsed_url = parse_url($fullUrl);
+
+// retrieve the query string
+$queryString = $parsed_url['query'];
+
+// Set switchAble session variable based on presence of ukey
+if ($ukey !== '') $_SESSION['switchAble'] = true;
+else $_SESSION['switchAble'] = false;
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+</head>
+<body>
     <main>
         <div class="container">
             <h1>Welcome </h1>
